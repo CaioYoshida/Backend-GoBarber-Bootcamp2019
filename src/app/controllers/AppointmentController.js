4efa767e845a772748e276972a5d83adf3a22f11
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-10-25 20:21:35 -0300

    chore: add Appointment's table and its configurations

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,82 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * Check if provider_id is an Id from a provider
+     */
+
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create an appointment with providers' });
+    }
+
+    /**
+     * Check for past dates
+     */
+
+    // 'parseISO()' tranforms a string into a date type
+    // 'startOfHour()' this function rounds time - like startOfHour(19:30:00) returns 19:00:00
+    const hourStart = startOfHour(parseISO(date));
+
+    // Now we have to compare if hourStar > date.now
+    // 'isBefore' function compares dates
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed' });
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    /**
+     * Creating an appointment
+     */
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
---
+++ src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // 'user_id' and 'provider_id' are appointment table's columns which need to be associated
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
